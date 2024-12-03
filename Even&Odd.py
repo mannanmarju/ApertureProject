62d976ecc947d344d355a6332cc871048a906fdb
Marzia Mannan <mannanmarju@gmail.com>
2024-12-03 15:29:19 -0800

    Adding new line

--- Even&Odd.py
+++ Even&Odd.py
@@ -3,10 +3,10 @@ for i in range(1, 11):
     if i % 2 == 0:  # Check if the number is even
         print(i)
 
-
+#Adding a new line
 
 print("\nOdd numbers:")
 for i in range(1, 11):
     if i % 2 != 0:  # Check if the number is odd
         print(i)
-    
+        
